3e3e832451c5603bb5a5257595be1febc6ac37d8
riux <riux.mend@gmail.com>
2019-02-19 23:34:18 -0500

    tests commit 2

--- src/main/java/com/edu/iestpch/app/InstitutoChurcampaApplication.java
+++ src/main/java/com/edu/iestpch/app/InstitutoChurcampaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InstitutoChurcampaApplication {
-
+//testttt
 	public static void main(String[] args) {
 		SpringApplication.run(InstitutoChurcampaApplication.class, args);
 	}
